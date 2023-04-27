24b4f0c316464f9a192fc2fb4313062eb05a78f9
SRE-NADH <sreenadh669@gmail.com>
2023-04-27 09:58:05 +0530

    work done

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -46,7 +46,7 @@ public class BankAccount {
     }
 
     public void withdraw(double amount) throws Exception {
-        if(balance-amount<minBalance){
+        if(balanceg-amount<minBalance){
             throw new Exception("Insufficient Balance");
         }
         balance-=amount;
